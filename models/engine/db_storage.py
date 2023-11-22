f0c3e69d41ad5bbec2a851ba01e7004fda9efe89
TPoe25 <t_poe25@yahoo.com>
2023-11-21 18:42:16 -0600

    fixing 7

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -70,4 +70,8 @@ class DBStorage:
         sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(sess_factory)
         self.__session = Session
-
+        
+        
+    def close(self):
+        """ Calls remove() method on the private session attribute"""
+        self.__session.remove()
